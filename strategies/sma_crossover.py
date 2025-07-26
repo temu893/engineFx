7bfada4c180649c58e05c8b54e7ec000cef4c4fb
temesgen <65918861+temu893@users.noreply.github.com>
2025-07-26 11:23:43 +0000

    back

--- strategies/sma_crossover.py
+++
@@ -1,9 +0,0 @@
-import backtrader as bt
-
-class SmaCross(bt.SignalStrategy):
-    params = dict(period=20)
-
-    def __init__(self):
-        sma = bt.ind.SMA(period=self.p.period)
-        crossover = bt.ind.CrossOver(self.data.close, sma)
-        self.signal_add(bt.SIGNAL_LONG, crossover)
